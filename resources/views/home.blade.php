3935950546ea5c8832ae8e15e981060bde5c02ac
Aizaz <aizazarifansari@gmail.com>
2019-07-21 13:34:17 +0500

    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row p-3">
+      <div class="col-3">
+        <img src="https://instagram.fisb6-1.fna.fbcdn.net/vp/bb0e6fe145176b6d69acbc50ee29c985/5DC8D9C8/t51.2885-19/s320x320/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fisb6-1.fna.fbcdn.net" class="rounded-circle" style="width: 150px; height:150px"/>
+      </div>
+      <div class="col-9">
+        <div><h1>Free Code Camp</h1></div>
+        <div class="d-flex">
+          <div class="pr-3"><strong class="pr-1">188</strong>posts</div>
+          <div class="pr-3"><strong class="pr-1">26.8k</strong>followers</div>
+          <div class="pr-3"><strong class="pr-1">224</strong>following</div>
+        </div>
+        <div class="pt-4"><strong> freecodecamp.org </strong> </div>
+        <div> blaj blah blah blah </div>
+        <div> <a href="#">freecodecamp.org</a></div>
+      </div>
+    </div>
+
+    <div class="row p-5">
+      <div class="col-4">
+        <img src="https://instagram.fisb6-1.fna.fbcdn.net/vp/f971710dd8d777b5bacedd6f5cdcca7a/5DB0B9C8/t51.2885-15/e35/c0.109.925.925a/s240x240/66056371_2243301639072331_4740487891029112385_n.jpg?_nc_ht=instagram.fisb6-1.fna.fbcdn.net" class="w-100">
+      </div>
+      <div class="col-4">
+        <img src="https://instagram.fisb6-1.fna.fbcdn.net/vp/f971710dd8d777b5bacedd6f5cdcca7a/5DB0B9C8/t51.2885-15/e35/c0.109.925.925a/s240x240/66056371_2243301639072331_4740487891029112385_n.jpg?_nc_ht=instagram.fisb6-1.fna.fbcdn.net" class="w-100">
+      </div>
+      <div class="col-4">
+        <img src="https://instagram.fisb6-1.fna.fbcdn.net/vp/f971710dd8d777b5bacedd6f5cdcca7a/5DB0B9C8/t51.2885-15/e35/c0.109.925.925a/s240x240/66056371_2243301639072331_4740487891029112385_n.jpg?_nc_ht=instagram.fisb6-1.fna.fbcdn.net" class="w-100">
+      </div>
+    </div>
+</div>
+@endsection
