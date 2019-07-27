b69ad54218929976ef5d557cc56bb5ddf41e1d11
Aizaz <aizazarifansari@gmail.com>
2019-07-27 23:47:08 +0500

    Edit Profile Functionality

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -16,15 +16,19 @@ class ProfilesController extends Controller
   }
 
   public function edit(User $user){ //another method to find using ID
+    $this->authorize('update',$user->profile);
     return view('profiles/edit',compact('user')); //compact does the same job as in the above method
   }
 
-  public function update(){
+  public function update(User $user){
     $data = request()->validate([
       'title' => 'required',
       'description' => 'required',
       'url' => 'url',
       'image' => 'image',
     ]);
+    //auth ensures we only edit authenticated user
+    auth()->user()->profile->update($data);
+    return redirect("/profile/{$user->id}");
   }
 }
--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $guarded = [];
     public function user(){ //looks for user_id from the Profiles model and compares to User class
       return $this->belongsTo(User::class);
     }
