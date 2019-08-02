b974c5db9603c4fbf8c68b1fd89bdc5eda27ff1e
Aizaz <aizazarifansari@gmail.com>
2019-08-03 00:02:17 +0500

    Home route fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                <a class="navbar-brand d-flex" href="{{ url('/home') }}">
                   <div><img src="/svg/icon.svg" style="height: 20px; border-right: 1px solid #333" class="pr-3"> </div>
                   <div class="pl-3 pt-0.5">Campus Life</div>
                 </a>
